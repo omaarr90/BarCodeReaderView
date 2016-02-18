601b48c98a4069fe27dc51105e98a917bc97f699
Omar Alshammari <omar.alshammary@hotmail.com>
2016-02-18 12:52:53 +0300

    Initial Commit

---
+++ BarCodeReader/BarCodeReader.h
@@ -0,0 +1,19 @@
+//
+//  BarCodeReader.h
+//  BarCodeReader
+//
+//  Created by Omar Alshammari on 2/18/16.
+//  Copyright © 2016 ___OALSHAMMARI___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BarCodeReader.
+FOUNDATION_EXPORT double BarCodeReaderVersionNumber;
+
+//! Project version string for BarCodeReader.
+FOUNDATION_EXPORT const unsigned char BarCodeReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarCodeReader/PublicHeader.h>
+
+
